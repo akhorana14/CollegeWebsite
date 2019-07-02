c144e149f268987cef90d3d0390ca1b53c48f37b
akhorana14 <31749972+akhorana14@users.noreply.github.com>
2019-07-02 11:24:36 -0500

    moved all methods into php file

--- WebContent/index.js
+++ WebContent/index.js
@@ -2,15 +2,4 @@
  * 
  */
 
-function onLogin() {
-	document.getElementById("overlay").style.display = "block";
-}
-function onRegister() {
-	document.getElementById("overlay").style.display = "block";
-}
-  
-function off() {
-  document.getElementById("overlay").style.display = "none";
-  window.history.back(); 
-}
  
